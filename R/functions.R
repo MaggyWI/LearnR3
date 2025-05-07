f9c17882b513cc2e3a5b9e5fb1c466b054ae5127
Ingrid Marguerite Wagnon <maggy.wagnon@googlemail.com>
2025-05-07 09:47:11 +0200

    End of exercise 8.5 alone

--- R/functions.R
+++ R/functions.R
@@ -16,3 +16,25 @@ import_dime <- function(file_path, n = 100) {
   return(data)
 }
 
+
+
+#' Import the csv files
+#'
+#' @param folder_path give the here("folder_path") where to look
+#'
+#' @returns tibble or data
+#'
+import_csv_files <- function(folder_path) {
+  files <- folder_path |>
+    fs::dir_ls(glob = "*.csv")
+  data <- files |>
+    purrr::map(import_dime) |>
+    purrr::list_rbind(names_to = "file_path_id")
+  return(data)
+}
+
+
+
+
+
+
